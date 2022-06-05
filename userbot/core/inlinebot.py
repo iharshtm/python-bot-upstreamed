1ab0e5b2640d3a71139cebd0aa5f3777bb28116f
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2022-06-05 14:53:56 +0000

    pylint: auto fixes

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -130,7 +130,7 @@ async def article_builder(event, method):
             media = random.choice(PIC)
 
     elif method == "ialive":
-        #thumb = get_thumb("alive")
+        # thumb = get_thumb("alive")
         buttons = [
             (
                 Button.inline("Stats", data="stats"),
@@ -231,7 +231,7 @@ async def article_builder(event, method):
         result = builder.document(
             media,
             title=title,
-            #thumb=thumb,
+            # thumb=thumb,
             description=description,
             text=query,
             buttons=buttons,
