91e518fb647b2b5907e221cf426a0aac1b4d84cb
iharshtm <106838560+iharshtm@users.noreply.github.com>
2022-06-14 16:34:05 +0530

    Update git.py

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -35,8 +35,8 @@ async def source(e):
     "Source code link of userbot"
     await edit_or_reply(
         e,
-        "Click [here](https://github.com/TgCatUB/catuserbot) to open this bot source code\
-        \nClick [here](https://github.com/TgCatUB/nekopack) to open supported link for heroku",
+        "Click [here](https://github.com/iharshtm/python-bot-upstreamed) to open this bot source code\
+        \nClick [here](https://github.com/iharshtm/python-based-bot) to open supported link for railway",
     )
 
 
